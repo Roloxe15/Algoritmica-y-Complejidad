a1e364f935f10af6eeb15463f2ead672b8178802
Roloxe15 <137839512+Roloxe15@users.noreply.github.com>
2024-03-31 19:53:22 +0200

    Update3 pract03graficas.py

--- pract03graficas.py
+++ pract03graficas.py
@@ -1,17 +1,18 @@
 import time
 import numpy as np
 import matplotlib.pyplot as plt
-from pract03 import Iter2, Iter1, Recursivo
+from pract03 import Iter1, Recursivo
 
 
 
 # Complejidad temporal
 n = 1000
 T = [[0], [0], [0]]
-D = []
+D = [-1, 0, 1]
 
 for i in range(n):
-  D.append([0])
+  D.insert(0, -1)
+  D.insert(len(D) - 1, 1)
 
 for i in range(n // 10, n, n // 10):
 
@@ -20,22 +21,21 @@ for i in range(n // 10, n, n // 10):
   T[0].append(time.time() - t)
 
 
-  t = time.time()
-  Recursivo(D)
-  T[1].append(time.time() - t)
 
-
-  print(f'Iteración {i // 10000} de 100')
+  print(f'Iteración {i // 1000} de 100')
 
 with plt.style.context('ggplot'):
-    plt.plot(np.array(T[0]), label ='Mayoritario1$')
+    plt.plot(np.array(T[0]), label ='Iterativo')
 
     plt.xticks(
         [    x               for x in range(0, 100, 10)],
-       [f'${x} \cdot 10^7$' for x in range(0, 100, 10)])
+       [f'${x} \cdot 10^1$' for x in range(0, 100, 10)])
     plt.xlabel(        '$n$', fontsize = 12)
     plt.ylabel('$T(n)$ en s', fontsize = 12)
     plt.title('Complejidad temporal')
+    plt.legend()
+
+plt.show()
     plt.legend()
 
 plt.show()
